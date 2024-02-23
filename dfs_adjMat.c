9e32476a3bb1dcd7148626c17f6034a5f00c617f
nik281 <agarwalnikhil281@gmail.com>
2024-02-23 22:48:57 +0530

    Changes in dfs

--- dfs_adjMat.c
+++ dfs_adjMat.c
@@ -3,15 +3,37 @@
 
 #define MAX_VERTICES 100
 
-// Function to perform DFS traversal
 void dfs(int adj_matrix[MAX_VERTICES][MAX_VERTICES], int num_vertices, int visited[MAX_VERTICES], int vertex) {
-    printf("%d ", vertex); // Print the current vertex
-    visited[vertex] = 1; // Mark the current vertex as visited
+    printf("%d ", vertex); 
+    visited[vertex] = 1; 
 
-    // Visit all adjacent vertices of the current vertex
+    
     for (int i = 0; i < num_vertices; i++) {
         if (adj_matrix[vertex][i] && !visited[i]) {
             dfs(adj_matrix, num_vertices, visited, i);
+        }
+    }
+}
+
+
+void bfs(int adj_matrix[MAX_VERTICES][MAX_VERTICES], int num_vertices, int visited[MAX_VERTICES], int start_vertex) {
+    int queue[MAX_VERTICES];
+    int front = -1, rear = -1;
+
+    printf("BFS traversal starting from vertex %d: ", start_vertex);
+    printf("%d ", start_vertex);
+    visited[start_vertex] = 1;
+    queue[++rear] = start_vertex;
+
+    while (front != rear) {
+        int current_vertex = queue[++front];
+
+        for (int i = 0; i < num_vertices; i++) {
+            if (adj_matrix[current_vertex][i] && !visited[i]) {
+                printf("%d ", i);
+                visited[i] = 1;
+                queue[++rear] = i;
+            }
         }
     }
 }
@@ -28,17 +50,34 @@ int main() {
     for (int i = 0; i < num_edges; i++) {
         int vertex1, vertex2;
         scanf("%d %d", &vertex1, &vertex2);
-        // Assuming input vertices are 0-indexed
+        
         adj_matrix[vertex1][vertex2] = 1;
-        adj_matrix[vertex2][vertex1] = 1; // Since the graph is undirected
+        adj_matrix[vertex2][vertex1] = 1; 
     }
 
     int start_vertex;
     printf("Enter the starting vertex: ");
     scanf("%d", &start_vertex);
 
-    printf("DFS traversal starting from vertex %d: ", start_vertex);
-    dfs(adj_matrix, num_vertices, visited, start_vertex);
+    int choice;
+    printf("Choose traversal method:\n1. DFS\n2. BFS\nEnter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            printf("DFS traversal starting from vertex %d: ", start_vertex);
+            dfs(adj_matrix, num_vertices, visited, start_vertex);
+            break;
+        case 2:
+            
+            for (int i = 0; i < num_vertices; i++) {
+                visited[i] = 0;
+            }
+            bfs(adj_matrix, num_vertices, visited, start_vertex);
+            break;
+        default:
+            printf("Invalid choice\n");
+    }
 
     return 0;
 }
